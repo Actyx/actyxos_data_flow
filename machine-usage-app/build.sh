28e0fd4225dd659591647ad44c75ed8d4f4073db
wngr <oliver@wngr.de>
2020-06-25 20:04:08 +0200

    Build examples using debian based docker container

--- machine-usage-app/build.sh
+++ machine-usage-app/build.sh
@@ -4,9 +4,14 @@ cd `dirname $0`
 
 FG='\033[0;35m'
 BG='\033[0m'
+pushd ..
+echo -e "${FG}building builder image${BG}"
+BUILDER_ID=`docker build -f Dockerfile.builder -q .`
+echo -e "${FG}built builder image ($BUILDER_ID)${BG}"
 
 echo -e "${FG}compiling with --release, this may take a while${BG}"
-cargo build --release --examples
+docker run -v `pwd`:/src -u $(id -u ):$(id -g) -it $BUILDER_ID cargo build --release --examples
+popd
 
 echo -e "${FG}copying binary${BG}"
 mkdir -pv bin
